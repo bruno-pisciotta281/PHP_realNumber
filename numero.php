628ed92053446affd5843f4cc4b2ebc8e989e6b1
Bruno Pisciotta <52466841+bruno-pisciotta281@users.noreply.github.com>
2023-07-14 17:38:53 -0300

    Add files via upload

---
+++ numero.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Resultado</title>
+</head>
+<body>
+    <main>
+        <h1>Analisador de Número Real</h1>
+        <?php 
+            $num = $_POST["n"] ?? 0; 
+            echo "<p>Analisando o número: <strong>". number_format($num, 3, ",", ".") ." </strong>informado.</p>";
+
+            $int = (int) $num ;
+            $fra = $num - $int ;
+
+            echo "<ul><li> A parte inteira do seu número é: <strong>". number_format($int, 3, ",", ".") ."</strong></li>";
+            echo "<li> A parte fracionária do seu número é: <strong>". number_format($fra, 3, ",", ".") ."</strong></li></ul>"
+        ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+</body>
+</html>
